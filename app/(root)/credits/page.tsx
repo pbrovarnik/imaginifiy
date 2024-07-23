df76a5bd5597e871961752d7fc9e2c4551972000
Pasha <pasha@getinkworks.com>
2024-07-23 15:01:59 -0400

    credits page styles

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -24,7 +24,7 @@ const Credits = async () => {
 				<ul className="credits-list">
 					{plans.map((plan) => (
 						<li key={plan.name} className="credits-item">
-							<div className="flex-center flex-col gap-3">
+							<div className="flex-center flex-col gap-2">
 								<Image src={plan.icon} alt="check" width={50} height={50} />
 								<p className="p-20-semibold mt-2 text-purple-500">{plan.name}</p>
 								<p className="h1-semibold text-dark-600">${plan.price}</p>
